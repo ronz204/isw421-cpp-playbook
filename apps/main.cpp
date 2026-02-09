1fa0635986b848c45f43743c8d59c46f0a84d15a
roney ruiz <roneyruizrojas@gmail.com>
2026-02-08 22:15:29 -0600

    feat: learn about vars and conditions

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  int alpha = 5;
+  int beta = 10;
+
+  int result = alpha + beta;
+  std::cout << "The result of adding alpha and beta is: " << result << std::endl;
+
+
+  int day = 3;
+  switch (day) {
+    case 1:
+      std::cout << "Monday" << std::endl;
+      break;
+    case 2:
+      std::cout << "Tuesday" << std::endl;
+      break;
+    case 3:
+      std::cout << "Wednesday" << std::endl;
+      break;
+    default:
+      std::cout << "Invalid day" << std::endl;
+      break;
+  }
+
   return 0;
 }
